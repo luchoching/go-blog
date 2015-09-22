5ab354c4b578d680ea0787b825749bc443ec87ad
Lu Cho-Ching <choching.work@gmail.com>
2015-09-22 14:45:55 +0800

    Add Index pge --> posts list

--- watch.go
+++ watch.go
@@ -15,11 +15,25 @@ type Post struct {
 	Body  template.HTML
 }
 
+type Index struct {
+	PostList []string
+}
+
 func loadPost(title string) *Post {
 	fileanme := title + ".md"
 	source, _ := ioutil.ReadFile(path.Join("posts", fileanme))
 	body := template.HTML(blackfriday.MarkdownCommon(source))
 	return &Post{title, body}
+}
+
+func loadPostList() *Index {
+	files, _ := ioutil.ReadDir("posts")
+	l := []string{}
+	for _, f := range files {
+		name := f.Name()
+		l = append(l, name[:len(name)-3])
+	}
+	return &Index{l}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +42,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, p)
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(path.Join("templates", "Index.html")))
+	t.Execute(w, loadPostList())
+}
+
 func main() {
 	http.HandleFunc("/posts/", handler)
+	http.HandleFunc("/", indexHandler)
 	if err := http.ListenAndServe(":4000", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
