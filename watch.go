5b1c99b5aeec356b0c62b10192bbf709c5b237cd
Lu Cho-Ching <choching.work@gmail.com>
2015-09-22 10:38:12 +0800

    Add Post struct and related templates

--- watch.go
+++ watch.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
 )
 
+type Post struct {
+	Title string
+	Body  string
+}
+
+func loadPost(title string) *Post {
+	fileanme := title + ".md"
+	body, _ := ioutil.ReadFile(path.Join("posts", fileanme))
+	fmt.Println(fileanme)
+	fmt.Println(body)
+	return &Post{title, string(body)}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	p := loadPost(r.URL.Path[1:])
 	t := template.Must(template.ParseFiles(path.Join("templates", "Post.html")))
-	t.Execute(w, nil)
+	t.Execute(w, p)
 }
 
 func main() {
